749fb8f9bfd3c286fa918c1883aa9c0be91278dc
Agnij Dutta <126397667+agnij-dutta@users.noreply.github.com>
2025-03-07 10:41:45 +0530

    Delete old client

--- client/app/metadata.ts
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "ZeedChain - Startup Equity NFT Platform",
-  description: "Decentralized platform for startup equity NFTs",
-  icons: {
-    icon: '/favicon.ico',
-  },
-};
